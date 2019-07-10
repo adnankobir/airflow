a549926eedebe609dd0b511fb9a9e700e6734a1a
Adnan Kobir <adnan.kobir@gmail.com>
2019-07-10 12:04:59 -0400

    drop fouth

--- example_k8s_executor.py
+++ example_k8s_executor.py
@@ -53,11 +53,6 @@ def print_stuff2():
     print("stuff2!")
     sleep(480)
 
-def use_airflow_binary():
-    rc = os.system("airflow -h")
-    assert rc == 0
-    sleep(480)
-
 # You don't have to use any special KubernetesExecutor configuration if you don't want to
 start_task = PythonOperator(
     task_id="start_task", python_callable=print_stuff, dag=dag
@@ -73,18 +68,10 @@ two_task = PythonOperator(
     task_id="two_task", python_callable=print_stuff2, dag=dag
 )
 
-# Limit resources on this operator/task with node affinity & tolerations
-three_task = PythonOperator(
-    task_id="three_task", python_callable=use_airflow_binary, dag=dag,
-    executor_config={
-        "KubernetesExecutor": {"request_memory": "128Mi",
-                               "limit_memory": "128Mi"}}
-)
-
 # Add arbitrary labels to worker pods
 four_task = PythonOperator(
-    task_id="four_task", python_callable=print_stuff, dag=dag,
+    task_id="four_task", python_callable=print_stuff2, dag=dag,
     executor_config={"KubernetesExecutor": {"labels": {"foo": "bar"}}}
 )
 
-start_task.set_downstream([one_task, two_task, three_task, four_task])
+start_task.set_downstream([one_task, two_task, three_task])

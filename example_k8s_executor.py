06d6f8b77a6d2ca7516933d7f77e1d9d57370da0
Adnan Kobir <adnan.kobir@gmail.com>
2019-07-10 11:25:43 -0400

    use main bin

--- example_k8s_executor.py
+++ example_k8s_executor.py
@@ -43,10 +43,9 @@ def print_stuff():
     print("stuff!")
 
 
-def use_zip_binary():
-    rc = os.system("zip")
+def use_airflow_binary():
+    rc = os.system("airflow -h")
     assert rc == 0
-
 
 # You don't have to use any special KubernetesExecutor configuration if you don't want to
 start_task = PythonOperator(
@@ -56,7 +55,29 @@ start_task = PythonOperator(
 # But you can if you want to
 one_task = PythonOperator(
     task_id="one_task", python_callable=print_stuff, dag=dag,
-    executor_config={"KubernetesExecutor": {"image": "apache/airflow:master-ci"}}
+    executor_config={"KubernetesExecutor": {"image": "airflow:latest"}}
 )
 
-start_task.set_downstream([one_task])
+# Use the airflow -h binary
+two_task = PythonOperator(
+    task_id="two_task", python_callable=use_airflow_binary, dag=dag,
+    executor_config={"KubernetesExecutor": {"image": "airflow:latest"}}
+)
+
+# Limit resources on this operator/task with node affinity & tolerations
+three_task = PythonOperator(
+    task_id="three_task", python_callable=print_stuff, dag=dag,
+    executor_config={
+        "KubernetesExecutor": {"request_memory": "128Mi",
+                               "limit_memory": "128Mi",
+                               "tolerations": tolerations,
+                               "affinity": affinity}}
+)
+
+# Add arbitrary labels to worker pods
+four_task = PythonOperator(
+    task_id="four_task", python_callable=print_stuff, dag=dag,
+    executor_config={"KubernetesExecutor": {"labels": {"foo": "bar"}}}
+)
+
+start_task.set_downstream([one_task, two_task, three_task, four_task])
